af5060e6d962e2f350193c024fee33815f694154
Nathan Burr <nathanburr1@gmail.com>
2017-11-27 07:01:37 -0600

    Initial Database Script

--- SQLStuff/setup.sql
+++ SQLStuff/setup.sql
@@ -13,16 +13,16 @@ DROP TABLE IF EXISTS user_watched_items;
 
 CREATE TABLE websites (
 	id	int NOT NULL,
-	name	varchar NOT NULL,
-	uri	varchar NOT NULL,
-	descr	varchar,
-	PRIMARY KEY(ID, name, uri),
+	name	varchar(20) NOT NULL,
+	uri	varchar(250) NOT NULL,
+	descr	varchar(400),
+	PRIMARY KEY(id),
 	UNIQUE (uri)
 );
 
 CREATE TABLE watchables (
 	id	int NOT NULL,
-	PRIMARY KEY(ID)
+	PRIMARY KEY(id)
 );
 
 CREATE TABLE watchables_sites (
@@ -32,20 +32,20 @@ CREATE TABLE watchables_sites (
 
 CREATE TABLE organizations (
 	id	int NOT NULL REFERENCES(watchables.id),
-	name	varchar NOT NULL,
-	email	varchar NOT NULL
+	name	varchar(20) NOT NULL,
+	email	varchar(40) NOT NULL
 );
 
 CREATE TABLE contributors (
 	id	int NOT NULL REFERENCES(watchables.id),
-	name	varchar NOT NULL,
-	email	varchar NOT NULL
+	name	varchar(20) NOT NULL,
+	email	varchar(40) NOT NULL
 );
 
 CREATE TABLE projects (
 	id		int NOT NULL REFERENCES(watchables.id),
-	name		varchar NOT NULL,
-	repo		varchar NOT NULL,
+	name		varchar(20) NOT NULL,
+	repo		varchar(2000) NOT NULL,
 	owner_id	int NOT NULL REFERENCES(organizations.id)
 );
 
@@ -57,31 +57,31 @@ CREATE TABLE download_mirrors (
 CREATE TABLE org_members (
 	org_id		int NOT NULL REFERENCES(organizations.id),
 	contributor_id	int NOT NULL REFERENCES(contributors.id),
-	role		varchar
+	role		varchar(15)
 );
 
 CREATE TABLE project_contributors (
 	project_id	int NOT NULL REFERENCES(projects.id),
 	contributor_id	int NOT NULL REFERENCES(contributors.id),
-	role		varchar
+	role		varchar(15)
 );
 
 CREATE TABLE licenses (
 	id		int NOT NULL,
-	name		varchar NOT NULL,
-	text_link	varchar NOT NULL REFERENCES(websites.id),
+	name		varchar(20) NOT NULL,
+	text_link	varchar(80) NOT NULL REFERENCES(websites.id),
 	UNIQUE(name, text_link),
 	PRIMARY KEY(id)
 );
 
 CREATE TABLE users (
 	id		int NOT NULL,
-	nickname	varchar NOT NULL,
-	name		varchar,
-	email		varchar NOT NULL,
-	pw_hash		varchar NOT NULL,
+	nickname	varchar(20) NOT NULL,
+	name		varchar(20),
+	email		varchar(40) NOT NULL,
+	pw_hash		varchar(50) NOT NULL,
 	UNIQUE(nickname),
-	PRIMARY KEY(id, email)
+	PRIMARY KEY(id)
 );
 
 CREATE TABLE user_watched_items (
